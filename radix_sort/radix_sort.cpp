d10bb8e86524ed9127fd57d77e451e7b5e2266b6
bat2492 <bat2492@tamu.edu>
2024-10-15 09:40:50 -0500

    added parallel data generation

---
+++ radix_sort/radix_sort.cpp
@@ -0,0 +1,38 @@
+#include <mpi.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <chrono>
+#include <random>
+#include <caliper/cali.h>
+#include <caliper/cali-manager.h>
+
+void generate_data(int* arr, int size, const char* type, int mpi_rank, int mpi_size) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(0, size - 1);
+
+    if (strcmp(type, "sorted") == 0) {
+        for (int i = 0; i < size; i++) {
+            arr[i] = i + mpi_rank*size;
+        }
+    } else if (strcmp(type, "reverse_sorted") == 0) {
+        for (int i = 0; i < size; i++) {
+            arr[i] = (mpi_size-mpi_rank)*size - i - 1;
+        }
+    } else if (strcmp(type, "random") == 0) {
+        for (int i = 0; i < size; i++) {
+            arr[i] = dis(gen);
+        }
+    } else if (strcmp(type, "perturbed") == 0) {
+        for (int i = 0; i < size; i++) {
+            arr[i] = i;
+        }
+        int perturb_count = size / 100;
+        for (int i = 0; i < perturb_count; i++) {
+            int idx = dis(gen);
+            arr[idx] = dis(gen);
+        }
+    }
+}
