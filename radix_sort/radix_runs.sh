b5a7053810fdc324a6f5d3629bbb13a857d3d057
bat2492 <bat2492@tamu.edu>
2024-10-22 22:52:11 -0500

    added bash script for bulk runs

---
+++ radix_sort/radix_runs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+processors=$1
+
+array_sizes=(16 18 20 22 24 26 28)
+
+input_type=("sorted" "reverse_sorted" "random" "perturbed")
+
+# Loop through each combination
+for size in "${array_sizes[@]}"; do
+    for type in "${input_type[@]}"; do
+        echo "Submitting job with array size 2^$size and $processors processors and input type $type"
+        sbatch "radix_sort.grace_job_${processors}" $size $processors $type
+        
+        sleep 5
+    done
+done
